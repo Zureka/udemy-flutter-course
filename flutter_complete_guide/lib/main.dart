20f96d9e907ca45905103d4d80455b159b1a99cc
Alex Zurek <zureka@mail.gvsu.edu>
2019-07-06 18:44:48 -0400

    Initial commit after section 2

---
+++ flutter_complete_guide/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+import './answer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex += 1;
+    });
+    print('Answer Chosen!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const questions = [
+      {
+        'questionText': 'What\'s your favorite color?',
+        'answers': [
+          'Black',
+          'Red',
+          'Green',
+          'White',
+        ],
+      },
+      {
+        'questionText': 'What\'s your favorite animal?',
+        'answers': [
+          'Rabbit',
+          'Snake',
+          'Elephant',
+          'Lion',
+        ],
+      },
+      {
+        'questionText': 'Who\'s your favorite instructor?',
+        'answers': [
+          'Max',
+          'Max',
+          'Max',
+          'Max',
+        ],
+      },
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Question(questions[_questionIndex]['questionText']),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answerText) => Answer(_answerQuestion, answerText))
+                .toList(),
+          ],
+        ),
+      ),
+    );
+  }
+}
