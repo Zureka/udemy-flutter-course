360ddbf27d8ea3cba4b7526190a69958c023ebe6
Alex Zurek <zureka@mail.gvsu.edu>
2019-08-04 09:41:43 -0400

    Use a ListTile for transacation items

--- expense_tracker/lib/widgets/transaction_list.dart
+++ expense_tracker/lib/widgets/transaction_list.dart
@@ -40,47 +40,31 @@ class TransactionList extends StatelessWidget {
 
   Widget _buildTransactionItem(BuildContext context, int index) {
     Transaction tx = transactions.elementAt(index);
-
     return Card(
-      child: Row(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.symmetric(
-              vertical: 10,
-              horizontal: 15,
-            ),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Theme.of(context).primaryColor,
-                width: 2,
-              ),
-            ),
-            padding: EdgeInsets.all(10.0),
-            child: Text(
-              '\$${tx.amount.toStringAsFixed(2)}',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Theme.of(context).primaryColor,
+      elevation: 5,
+      margin: EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 5,
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          child: Padding(
+            padding: const EdgeInsets.all(6),
+            child: FittedBox(
+              child: Text(
+                '\$${tx.amount.toStringAsFixed(2)}',
               ),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                tx.title,
-                style: Theme.of(context).textTheme.title,
-              ),
-              Text(
-                DateFormat.yMMMd().format(tx.date),
-                style: TextStyle(
-                  color: Colors.grey,
-                ),
-              )
-            ],
-          )
-        ],
+        ),
+        title: Text(
+          tx.title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        subtitle: Text(
+          DateFormat.yMMMd().format(tx.date),
+        ),
       ),
     );
   }
