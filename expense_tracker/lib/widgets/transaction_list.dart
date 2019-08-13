fd25594e13b2b0a80f2b08ab5ff903fc26cebe3e
Alex Zurek <zureka@mail.gvsu.edu>
2019-08-12 20:04:01 -0400

    Adds delete transaction

--- expense_tracker/lib/widgets/transaction_list.dart
+++ expense_tracker/lib/widgets/transaction_list.dart
@@ -5,8 +5,9 @@ import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
+  final Function deleteTx;
 
-  TransactionList(this.transactions);
+  TransactionList(this.transactions, this.deleteTx);
 
   @override
   Widget build(BuildContext context) {
@@ -64,6 +65,13 @@ class TransactionList extends StatelessWidget {
         ),
         subtitle: Text(
           DateFormat.yMMMd().format(tx.date),
+        ),
+        trailing: IconButton(
+          icon: Icon(
+            Icons.delete,
+            color: Theme.of(context).errorColor,
+          ),
+          onPressed: () => deleteTx(tx.id),
         ),
       ),
     );
