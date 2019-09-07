58193e4951878a0767356475fbf7da1bca570a10
Alex Zurek <zureka@mail.gvsu.edu>
2019-09-07 11:53:42 -0400

    Use static routeName property on class

--- deli_meals/lib/category_meals_screen.dart
+++ deli_meals/lib/category_meals_screen.dart
@@ -4,7 +4,8 @@ class CategoryMealsScreen extends StatelessWidget {
   // final String categoryId;
   // final String categoryTitle;
 
-  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = "/category-meals";
 
   @override
   Widget build(BuildContext context) {
